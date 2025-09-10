9d7ec343c8e3fd176408c631e84787b78305ae36
Lee, Seungjin <quack337@gmail.com>
2025-09-10 16:48:58 +0900

    Main.java 수정

--- src/git__test2/Main.java
+++ src/git__test2/Main.java
@@ -3,8 +3,8 @@ package git__test2;
 public class Main {
 
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
+        Hello hello = new Hello();
+        hello.say();
     }
 
 }
